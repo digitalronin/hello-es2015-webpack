1a820762610fc82fd932fd73be72302b5648369b
David Salgado <david@digitalronin.com>
2017-02-04 13:30:23 +0000

    add the html plugin and configure to use index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/app.js',
   output: {
     filename: 'application.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })]
 }
